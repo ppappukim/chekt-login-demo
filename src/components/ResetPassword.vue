24b9be80c9ccdc7f6b861288bdf9857adfe2b542
ppappukim <62290339+ppappukim@users.noreply.github.com>
2021-06-02 19:28:39 +0900

    TESTing reset password

--- src/components/ResetPassword.vue
+++ src/components/ResetPassword.vue
@@ -144,8 +144,7 @@ export default {
   watch: {
     resetEmailVerifyStatus: function () {
       console.log(this.resetEmailVerifyStatus);
-      if (this.resetEmailVerifyStatus === 'successful') this.isResetPasswordSuccess = true
-      else this.isExpired = true
+      if (this.resetEmailVerifyStatus !== 'successful') this.isExpired = true
     },
     // resetEmailConfirmStatus: function () {
     //   console.log(this.resetEmailConfirmStatus.message);
