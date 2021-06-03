15c31357354737d7a14bb5171d013e40d2327618
ppappukim <62290339+ppappukim@users.noreply.github.com>
2021-06-03 15:42:43 +0900

    TESTing reset password

---
+++ src/components/PasswordlessExpired.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="body">
+    <div class="form">
+      <div class="form-header">
+        <div class="title">This password reset link is invalid.</div>
+        <div class="desc">
+          {{descMessage}}
+        </div>
+        <div v-on:click="clickResetting()" class="action">
+          Try resetting your password again.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyIcon from '@/components/MyIcon'
+export default {
+  components: {
+    MyIcon,
+  },
+  name: 'resetpassword',
+  data () {
+    return {
+      descMessage: 'Currently, the link is not available. Please resetting your password again.',
+    }
+  },
+  mounted: function () {
+    if (!this.resetEmailVerifyStatus) return 
+    this.descMessage = this.resetEmailVerifyStatus.message
+  },
+  computed: {
+    resetEmailVerifyStatus: function () {
+      return this.$store.getters.resetEmailVerifyStatus
+    },
+  },
+  watch: {
+    resetEmailVerifyStatus: function () {
+      if (this.resetEmailVerifyStatus !== 'successful') this.descMessage = this.resetEmailVerifyStatus.message
+    },
+  },
+  methods: {
+    clickResetting: function () {
+      this.$router.push({path:'/forgotpassword'})
+    },
+    wait: function (time) {
+      return new Promise(resolve => {
+        setTimeout(() => { resolve() }, time)
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+.form {
+  margin: 60px 60px 100px 60px;
+}
+.body {
+  box-shadow: 0px 0px 20px 0px #e3e8ee6d;
+  border: solid 1px var(--blue-gray-medium);
+  border-radius: 5px;
+}
+.title {
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--gray-high);
+  text-align: start;
+}
+
+.desc {
+  margin-top:10px;
+  font-size: 14px;
+  color: var(--blue-gray-higher);
+  line-height: 18px;
+}
+
+.action {
+  margin-top:20px;
+  font-size: 14px;
+  color: var(--primary);
+  font-weight: 400;
+  cursor: pointer;
+}
+.action:hover {
+  color: var(--gray-high);
+}
+
+
+/****** media ******/
+@media screen and (max-width: 600px) {
+  .avatar {
+    display: none;
+  }
+  .form {
+    margin: 60px 30px 100px 30px;
+  }
+}
+</style>
