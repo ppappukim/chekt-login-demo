a60d130cdb8274444457f3ee563465e9a4e8b562
ppappukim <62290339+ppappukim@users.noreply.github.com>
2021-06-04 15:23:25 +0900

    TESTing reset password

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ export default new Router({
         },
         {
           path: '/resetpassword_expired',
-          component: ResetPassword
+          component: PasswordlessExpired
         },
         {
           path: '/passwordless',
