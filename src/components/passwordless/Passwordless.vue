84fe4b37cbe835b6008315de82a49dd6e00731be
ppappukim <62290339+ppappukim@users.noreply.github.com>
2021-06-04 17:48:40 +0900

    TESTing reset password

--- src/components/passwordless/Passwordless.vue
+++ src/components/passwordless/Passwordless.vue
@@ -133,7 +133,7 @@ export default {
         // The link was successfully sent. Inform the user.
         // Save the email locally so you don't need to ask the user for it again
         // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
+        window.localStorage.setItem('emailForSignIn', this.email);
         this.isSend = true
         this.codeInputFocus()
       } catch (err) {
