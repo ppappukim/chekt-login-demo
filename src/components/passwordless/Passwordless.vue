232a25ddee00421eb2bc6b0c503d4da31ccdc275
ppappukim <62290339+ppappukim@users.noreply.github.com>
2021-06-04 17:45:53 +0900

    TESTing reset password

--- src/components/passwordless/Passwordless.vue
+++ src/components/passwordless/Passwordless.vue
@@ -130,6 +130,10 @@ export default {
       // SEND PASSWORDLESS LINK ACTION
       try {
         await this.$firebase.auth.sendSignInLinkToEmail(this.email)
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email);
         this.isSend = true
         this.codeInputFocus()
       } catch (err) {
